de8265f1ba1d69ec00ac33260d57d63d8d07b73f
Matos52 <Matej.Regec@gmail.com>
2024-04-20 22:47:13 +0200

    Continue with development of application.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,24 +1,50 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
-import quizTrophyImg from "../assets/quiz-complete.png"
+import quizTrophyImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleUserAnswer(selectedAnswer) {
-    setUserAnswers(prevUserAnswers => {
-        return [...prevUserAnswers, selectedAnswer]
-    });
-  }
+  const handleUserAnswer = useCallback(
+    function handleUserAnswer(selectedAnswer) {
+      setAnswerState("answered");
 
-  if(quizIsComplete) {
-    return <div id="summary">
-        <img src={quizTrophyImg} alt="Trophy icon"/>
+      setUserAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState('');
+        }, 2000)
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
+
+  const handleSkipAnswer = useCallback(
+    () => handleUserAnswer(null),
+    [handleUserAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizTrophyImg} alt="Trophy icon" />
         <h2>Quiz Completed!</h2>
-    </div>
+      </div>
+    );
   }
 
   const sortedAnswers = [...QUESTIONS[activeQuestionIndex].answers];
@@ -27,13 +53,31 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-            {sortedAnswers.map((answer) => (
-                <li key={answer} className="answer">
-                    <button onClick={() => handleUserAnswer(answer)}>{answer}</button>
-                </li>
-            ))}
+          {sortedAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClass = '';
+
+            if(answerState === 'answered' && isSelected) {
+              cssClass = 'selected';
+            }
+
+            if((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+              cssClass = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button onClick={() => handleUserAnswer(answer)} className={cssClass}>{answer}</button>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </div>
